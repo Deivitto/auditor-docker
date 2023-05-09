5f478875aa6438e603c1601a8da367eaf28292c4
Ðavid <dcm62@alu.ua.es>
2023-05-09 18:37:01 +0200

    Update echidna_installer.sh
    Removed sudo as it fails installing
    Takes ages to install, look for optimizations

--- scripts/echidna_installer.sh
+++ scripts/echidna_installer.sh
@@ -4,10 +4,10 @@
     
     export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
     export HOMEBREW_REPOSITORY="$HOMEBREW_PREFIX/Homebrew"
-    sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
     (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/whitehat/.bashrc  && \
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && \
-    sudo brew install --HEAD echidna && \
-    sudo brew postinstall echidna
+    brew install --HEAD echidna && \
+    brew postinstall echidna
     
     echo "Installation completed!"
