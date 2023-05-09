af4363be376f06cce1a3309797fcc8a46aa017fa
Ðavid <dcm62@alu.ua.es>
2023-05-09 18:05:50 +0200

    Update installer.sh
    Different order for most typical used scripts

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -33,20 +33,20 @@ run_with_progress() {
 while true; do
 exec 3>&1
 selection=$(dialog \
---backtitle "Auditor Toolbox Installer" \
---title "Menu" \
---clear \
---cancel-label "Exit" \
---menu "Please select an option:" 0 0 7 \
-"1" "Install Python Developer Tools" \
-"2" "Install Certora Prover + Java SDK 11 (requirement)" \
-"3" "Install Manticore + Etheno" \
-"4" "Install Noir (Nargo) (Needs Cargo (6))" \
-"5" "Install Circom" \
-"6" "Install Cargo + Foundry" \
-"7" "Install Echidna" \
-"8" "Install Mythril" \
-2>&1 1>&3)
+    --backtitle "Auditor Toolbox Installer" \
+    --title "Menu" \
+    --clear \
+    --cancel-label "Exit" \
+    --menu "Please select an option:" 0 0 8 \
+    "1" "Install Cargo + Foundry" \
+    "2" "Install Echidna" \
+    "3" "Install Certora Prover + Java SDK 11 (requirement)" \
+    "4" "Install Mythril" \
+    "5" "Install Manticore + Etheno" \
+    "6" "Install Noir (Nargo) (Needs Cargo (1))" \
+    "7" "Install Circom" \
+    "8" "Install Python Developer Tools" \
+    2>&1 1>&3)
 exit_code=$?
 exec 3>&-
 case $exit_code in
@@ -61,51 +61,52 @@ echo "Installation canceled."
 exit
 ;;
 esac
-    case $selection in
+case $selection in
     0)
-    clear
-    echo "Installation canceled."
-    exit
+        clear
+        echo "Installation canceled."
+        exit
     ;;
     1)
-        run_with_progress "/home/whitehat/scripts/py_developer_setup.sh" "Python Developer Tools"
-        result="py_developer_setup.sh installed successfully!"
-        display_result "Result"
-    ;;
-    2)
-        run_with_progress "/home/whitehat/scripts/certora_setup.sh" "Certora Prover + Java SDK 11"
-        result="certora_setup.sh installed successfully!"
-        display_result "Result"
-    ;;
-    3)
-        run_with_progress "/home/whitehat/scripts/advanced_tob_tools_setup.sh" "Manticore + Etheno"
-        result="advanced_tob_tools_setup.sh installed successfully!"
-        display_result "Result"
-    ;;
-    4)
-        run_with_progress "/home/whitehat/scripts/noir_setup.sh" "Noir (Nargo)"
-        result="noir_setup.sh installed successfully!"
-        display_result "Result"
-    ;;
-    5)
-        run_with_progress "/home/whitehat/scripts/circom_setup.sh" "Circom"
-        result="circom_setup.sh installed successfully!"
-        display_result "Result"
-    ;;
-    6)
         run_with_progress "/home/whitehat/scripts/cargo_foundry_installer.sh" "Cargo + Foundry"
         result="cargo_foundry_installer.sh installed successfully!"
         display_result "Result"
     ;;
-    7)
+    2)
         run_with_progress "/home/whitehat/scripts/echidna_installer.sh" "Echidna"
         result="echidna_installer.sh installed successfully!"
         display_result "Result"
     ;;
-    8)
+    3)
+        run_with_progress "/home/whitehat/scripts/certora_setup.sh" "Certora Prover + Java SDK 11"
+        result="certora_setup.sh installed successfully!"
+        display_result "Result"
+    ;;
+    4)
         run_with_progress "/home/whitehat/scripts/mythril_install.sh" "Mythril"
         result="mythril_install.sh installed successfully!"
         display_result "Result"
     ;;
+    5)
+        run_with_progress "/home/whitehat/scripts/advanced_tob_tools_setup.sh" "Manticore + Etheno"
+        result="advanced_tob_tools_setup.sh installed successfully!"
+        display_result "Result"
+    ;;
+    6)
+        run_with_progress "/home/whitehat/scripts/noir_setup.sh" "Noir (Nargo)"
+        result="noir_setup.sh installed successfully!"
+        display_result "Result"
+    ;;
+    7)
+        run_with_progress "/home/whitehat/scripts/circom_setup.sh" "Circom"
+        result="circom_setup.sh installed successfully!"
+        display_result "Result"
+    ;;
+    8)
+        run_with_progress "/home/whitehat/scripts/py_developer_setup.sh" "Python Developer Tools"
+        result="py_developer_setup.sh installed successfully!"
+        display_result "Result"
+    ;;
 esac
+
 done
