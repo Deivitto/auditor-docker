1578a74dd6ab887bf236ac209bc9ebdcca9eadd0
Ðavid <dcm62@alu.ua.es>
2023-05-09 21:14:09 +0200

    Update advanced_tob_tools_setup.sh

--- scripts/advanced_tob_tools_setup.sh
+++ scripts/advanced_tob_tools_setup.sh
@@ -1,1 +1,5 @@
-python3.9 -m pip install     manticore     etheno     "eth-abi>=4.0.0"     "eth-account>=0.8.0"     "eth-hash[pycryptodome]>=0.5.1"     "eth-typing>=3.0.0"     "eth-utils>=2.1.0"     "crytic-compile>=0.3.1,<0.4.0"     "web3>=6.0.0"
+python3.9 -m pip install     manticore     etheno     "eth-abi>=4.0.0"     "eth-account>=0.8.0"     "eth-hash[pycryptodome]>=0.5.1"     "eth-typing>=3.0.0"     "eth-utils>=2.1.0"     "crytic-compile>=0.3.1,<0.4.0"     "web3>=6.0.0" && \
+# Install global npm packages
+npm install --omit=dev --global --force \
+    embark \
+    @trailofbits/embark-contract-info \
