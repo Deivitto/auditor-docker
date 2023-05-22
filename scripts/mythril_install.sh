ebc42d602d7cc2a26c411c4cd9073d40dbbbe24c
Ðavid <dcm62@alu.ua.es>
2023-05-23 01:14:59 +0200

    Update mythril_install.sh
    myth update to make it work

--- scripts/mythril_install.sh
+++ scripts/mythril_install.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
     cd /home/whitehat
     
-    python3.9 -m venv mythril_env
+    python3.9 -m venv .mythril_env
     
-    source mythril_env/bin/activate
+    source .mythril_env/bin/activate
     
     pip install mythril
     
     deactivate
     
-    echo "alias myth='source $(pwd)/mythril_env/bin/activate && myth \$@ && deactivate'" >> ~/.bashrc
-    
+echo -e 'function myth() {\n\
+    source $(pwd)/.mythril_env/bin/activate\n\
+    command myth "$@"\n\
+    deactivate\n\
+}' >> ~/.bashrc
+
     source ~/.bashrc
     
     echo "Mythril installation complete. Use the 'myth' command to run Mythril within the virtual environment."
