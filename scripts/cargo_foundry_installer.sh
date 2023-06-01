9f169d61af3635d2cd22f4074c7297eac58a73b9
Ðavid <dcm62@alu.ua.es>
2023-06-01 22:18:20 +0200

    Update cargo_foundry_installer.sh
    fix for UX, now, launching the command should finish launching foundryup, and therefore, after installation forge and foundryup should be available in the cli

--- scripts/cargo_foundry_installer.sh
+++ scripts/cargo_foundry_installer.sh
@@ -1,1 +1,7 @@
-curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y &&     curl -L https://foundry.paradigm.xyz | bash &&     export PATH="~/.foundry/bin:${PATH}" &&   source ~/.bashrc &&  foundryup
+#!/bin/bash
+
+curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+curl -L https://foundry.paradigm.xyz | bash
+export PATH="~/.foundry/bin:${PATH}"
+source ~/.bashrc
+foundryup
