8142d86fe1f1731516c21fa2a9f73ae2d323306a
Ðavid <dcm62@alu.ua.es>
2023-06-02 00:21:33 +0200

    Fix of circom insstallation

--- scripts/circom_setup.sh
+++ scripts/circom_setup.sh
@@ -1,1 +1,6 @@
-sudo npm install --omit=dev --global --force circom
+#!/bin/bash
+git clone https://github.com/iden3/circom.git .circom
+cd .circom
+cargo build --release
+cargo install --path circom
+npm install -g --omit=dev --global --force snarkjs 
