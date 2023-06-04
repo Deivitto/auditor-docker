4653ab505a1dc92105b854cf3e61500ad708e5ac
Ðavid <dcm62@alu.ua.es>
2023-06-04 12:30:31 +0200

    Create vscode_audit_extensions.sh
    Added script for people who doesn't have vscode extensions

---
+++ scripts/vscode_audit_extensions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Substitute of juanfranblanco/vscode-solidity. Audit hovers, go to reference, etc
+code --install-extension ackeeblockchain.tools-for-solidity
+# Some packages from Ethereum Security Bundle
+code --install-extension ryu1kn.partial-diff
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension tintinweb.graphviz-interactive-preview
+code --install-extension tintinweb.solidity-metrics
+code --install-extension tintinweb.solidity-visual-auditor
+code --install-extension tintinweb.vscode-ethover
+code --install-extension tintinweb.vscode-inline-bookmarks
+code --install-extension tintinweb.vscode-LLL
+code --install-extension tintinweb.vscode-solidity-flattener
+code --install-extension tintinweb.vscode-solidity-language
+code --install-extension tintinweb.vscode-vyper
+# Git
+code --install-extension eamodio.gitlens
+code --install-extension gimenete.github-linker
+# See diagrams
+code --install-extension jebbs.plantuml
+code --install-extension joaompinto.vscode-graphviz
+# Notes
+code --install-extension tkrkt.linenote
+# Testing / Scripts
+code --install-extension eg2.vscode-npm-script
