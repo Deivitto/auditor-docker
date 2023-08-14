aeea5deec2950fd5410a55d8bf05b8afdd33425f
Ðavid <info.dcm62@gmail.com>
2023-08-15 00:36:16 +0200

    Update py_developer_setup.sh

--- scripts/py_developer_setup.sh
+++ scripts/py_developer_setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# Update dependency
-pip install --upgrade asttokens
-# Installs Vyper, Ape Vyper, Py-solc-x, Pyevwasm
-python3.9 -m pip install ape-vyper \ 
-pyevmasm \
-py-solc-x \
-vyper
+
+# Ensure we have a clean start
+pip3 uninstall -y asttokens vyper pyevmasm py-solc-x ape-vyper
+
+# Install asttokens at version 2.0.5 first as required by Vyper
+pip3 install asttokens==2.0.5
+
+# Then install Vyper and the other packages
+python3.9 -m pip install vyper ape-vyper pyevmasm py-solc-x
+
