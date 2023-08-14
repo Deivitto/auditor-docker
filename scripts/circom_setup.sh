4415becf6fad6b5bd99f1573d5e820a9b684bbf1
Ðavid <info.dcm62@gmail.com>
2023-08-14 12:40:28 +0200

    Update comments at circom_setup.sh

--- scripts/circom_setup.sh
+++ scripts/circom_setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Go home
 cd /home/whitehat
+# Clone circom into .circom
 git clone https://github.com/iden3/circom.git .circom
+# Cd in
 cd .circom
+# Launch circom commands
 cargo build --release
 cargo install --path circom
+# Install snarkjs
 npm install -g --omit=dev --global --force snarkjs 
