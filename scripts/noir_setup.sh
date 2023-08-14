4d90883a22be367942ea9265d0e3b25c217eafc2
Ðavid <info.dcm62@gmail.com>
2023-08-14 12:49:16 +0200

    Update comments at noir_setup.sh

--- scripts/noir_setup.sh
+++ scripts/noir_setup.sh
@@ -1,1 +1,12 @@
-source ~/.cargo/env &&     mkdir -p $HOME/.nargo/bin &&     curl -o $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -L https://github.com/noir-lang/noir/releases/download/v0.4.1/nargo-x86_64-unknown-linux-gnu.tar.gz &&     tar -xvf $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.nargo/bin/ &&     echo -e "\nexport PATH=$PATH:$HOME/.nargo/bin" >> ~/.bashrc &&     source ~/.bashrc
+# Installs Noir in .nargo folder
+source ~/.cargo/env && \
+# Creates the bin folder
+mkdir -p $HOME/.nargo/bin && \
+# Download the tar
+curl -o $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -L https://github.com/noir-lang/noir/releases/download/v0.4.1/nargo-x86_64-unknown-linux-gnu.tar.gz && \
+# Uncompress it
+tar -xvf $HOME/.nargo/bin/nargo-x86_64-unknown-linux-gnu.tar.gz -C $HOME/.nargo/bin/ && \
+# Save into the path
+echo -e "\nexport PATH=$PATH:$HOME/.nargo/bin" >> ~/.bashrc && \
+# Source to use nargo command
+source ~/.bashrc
