ab63b609ed5b6494cf2e51add8b9e2975c5b33af
Ðavid <info.dcm62@gmail.com>
2023-08-14 12:38:26 +0200

    Totally fixed mythril
    there was an issue with pip version, so when running pip install mythril was failing until i launched pip install --upgrade pip

--- scripts/mythril_install.sh
+++ scripts/mythril_install.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
-    cd /home/whitehat
+# Go home
+cd /home/whitehat
+
+# Create a venv just for mythril
+python3.9 -m venv .mythril_env
+
+# Source the env
+source .mythril_env/bin/activate
     
-    python3.9 -m venv .mythril_env
-    
-    source .mythril_env/bin/activate
-    
-    pip install mythril
-    
-    deactivate
-    
+# This next line fixes some wrong installations
+pip install --upgrade pip 
+
+# Install mythril
+pip install mythril
+
+# Deactivate after use
+deactivate
+
+# Create an "alias" what is actually a function in the bashrc so the whole process of using the venv is transparent
 echo -e 'function myth() {\n
     source $(pwd)/.mythril_env/bin/activate\n
     command myth "$@"\n
     deactivate\n
 }' >> ~/.bashrc
 
-    source ~/.bashrc
-    
-    echo "Mythril installation complete. Use the 'myth' command to run Mythril within the virtual environment."
+# Source bashrc for the use cases
+source ~/.bashrc
+
+# Installed
+echo "Mythril installation complete. Use the 'myth' command to run Mythril within the virtual environment."
     
