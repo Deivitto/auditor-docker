c583f239417fbdd0e5e18a2f805b704c2ed933a6
Ðavid <info.dcm62@gmail.com>
2023-08-15 01:10:13 +0200

    Fix bug regarding manticore
    After some testing, I notice that installing old dependencies solved the issue I was noticing in manticore and etheno.
    
    So both are now inside a venv available under manticore and etheno commands, or just by sourcing the .manticore_env
    
    Issue:
    ```
    Traceback (most recent call last):
      File "/home/whitehat/.local/bin/etheno", line 5, in <module>
        from etheno.__main__ import main
      File "/home/whitehat/.local/lib/python3.9/site-packages/etheno/__main__.py", line 23, in <module>
        from .manticoreclient import ManticoreClient
      File "/home/whitehat/.local/lib/python3.9/site-packages/etheno/manticoreclient.py", line 25, in <module>
        import manticore.utils
      File "/home/whitehat/.local/lib/python3.9/site-packages/etheno/manticoreclient.py", line 20, in manticoreimport
        return oldimport(name, *args, **kwargs)
      File "/home/whitehat/.local/lib/python3.9/site-packages/manticore/__init__.py", line 10, in <module>
        from .ethereum.manticore import ManticoreEVM
      File "/home/whitehat/.local/lib/python3.9/site-packages/etheno/manticoreclient.py", line 20, in manticoreimport
        return oldimport(name, *args, **kwargs)
      File "/home/whitehat/.local/lib/python3.9/site-packages/manticore/ethereum/__init__.py", line 3, in <module>
        from .manticore import ManticoreEVM, config
      File "/home/whitehat/.local/lib/python3.9/site-packages/etheno/manticoreclient.py", line 20, in manticoreimport
        return oldimport(name, *args, **kwargs)
      File "/home/whitehat/.local/lib/python3.9/site-packages/manticore/ethereum/manticore.py", line 15, in <module>
        from ..core.manticore import ManticoreBase, ManticoreError
      File "/home/whitehat/.local/lib/python3.9/site-packages/etheno/manticoreclient.py", line 20, in manticoreimport
        return oldimport(name, *args, **kwargs)
      File "/home/whitehat/.local/lib/python3.9/site-packages/manticore/core/manticore.py", line 29, in <module>
        from .worker import (
      File "/home/whitehat/.local/lib/python3.9/site-packages/etheno/manticoreclient.py", line 20, in manticoreimport
        return oldimport(name, *args, **kwargs)
      File "/home/whitehat/.local/lib/python3.9/site-packages/manticore/core/worker.py", line 4, in <module>
        from .state_pb2 import StateList, MessageList, State, LogMessage
      File "/home/whitehat/.local/lib/python3.9/site-packages/etheno/manticoreclient.py", line 20, in manticoreimport
        return oldimport(name, *args, **kwargs)
      File "/home/whitehat/.local/lib/python3.9/site-packages/manticore/core/state_pb2.py", line 32, in <module>
        _descriptor.EnumValueDescriptor(
      File "/home/whitehat/.local/lib/python3.9/site-packages/google/protobuf/descriptor.py", line 796, in __new__
        _message.Message._CheckCalledFromGeneratedFile()
    TypeError: Descriptors cannot not be created directly.
    If this call came from a _pb2.py file, your generated code is out of date and must be regenerated with protoc >= 3.19.0.
    If you cannot immediately regenerate your protos, some other possible workarounds are:
     1. Downgrade the protobuf package to 3.20.x or lower.
     2. Set PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python (but this will use pure-Python parsing and will be much slower).
    
    More information: https://developers.google.com/protocol-buffers/docs/news/2022-05-06#python-updates
    ```

--- scripts/advanced_tob_tools_setup.sh
+++ scripts/advanced_tob_tools_setup.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
-
+# Go home
 cd /home/whitehat
 
+mkdir .manticore_env
 # Create a virtual environment for Manticore
-python3 -m venv .manticore_env
+python3.9 -m venv .manticore_env
 
 # Activate the virtual environment
 source .manticore_env/bin/activate
 
 # Install Manticore with native dependencies
-pip3 install manticore[native]
-
-python3.9 -m pip install     manticore     etheno     "eth-abi>=4.0.0"     "eth-account>=0.8.0"     "eth-hash[pycryptodome]>=0.5.1"     "eth-typing>=3.0.0"     "eth-utils>=2.1.0"     "crytic-compile>=0.3.1,<0.4.0"     "web3>=6.0.0" && \
-# Install global npm packages
-npm install --omit=dev --global --force \
-    embark \
-    @trailofbits/embark-contract-info \
+pip3 install manticore[native] etheno eth-brownie
 
 # Deactivate the virtual environment after installation
 deactivate
 
 # Append a function to .bashrc for easy usage of Manticore within its virtual environment
-echo -e 'function mcore() {\n\
-    source $(pwd)/.manticore_env/bin/activate\n\
-    command manticore "$@"\n\
-    deactivate\n\
+echo -e 'function manticore() {\n
+    source $(pwd)/.manticore_env/bin/activate\n
+    command manticore "$@"\n
+    deactivate\n
 }' >> ~/.bashrc
+
+echo -e 'function etheno() {\n
+    source $(pwd)/.manticore_env/bin/activate\n
+    command etheno "$@"\n
+    deactivate\n
+}' >> ~/.bashrc
+
 
 # Source .bashrc to reflect the changes
 source ~/.bashrc
 
-echo "Manticore installation complete. Use the 'mcore' command to run Manticore within the virtual environment."
-
-
-
+echo "Manticore installation complete. Use the 'manticore' command to run Manticore within the virtual environment."
+echo "Etheno installed too. Globally installing embark"
 # Install global npm packages
 npm install --omit=dev --global --force \
     embark \
