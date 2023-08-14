af407e696302e0ccf94767b6d6d83b5ae37126ab
Ðavid <info.dcm62@gmail.com>
2023-08-14 12:39:26 +0200

    Update comments at certora_key_setup.sh

--- scripts/certora_key_setup.sh
+++ scripts/certora_key_setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-    
-    export CERTORAKEY=$1
-    
-    echo "export CERTORAKEY=\$CERTORAKEY" >> ~/.bashrc
-    
-    source ~/.bashrc
-    
-    echo "Certora key has been set up."
+
+# First param will be the key
+export CERTORAKEY=$1
+
+# Export to bashrc the cerora key
+echo "export CERTORAKEY=\$CERTORAKEY" >> ~/.bashrc
+
+# Source it
+source ~/.bashrc
+
+# Confirmation message
+echo "Certora key has been set up."
     
