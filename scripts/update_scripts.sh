15c2fee3a987ed11ec2b8a4e4f4230460c4a6b7f
Ðavid <info.dcm62@gmail.com>
2023-08-14 23:52:10 +0200

    Updated update scripts to also update the templates folder
    Makes sense to also add support of new templates and keep it as a "Updater" in general

--- scripts/update_scripts.sh
+++ scripts/update_scripts.sh
@@ -1,63 +1,65 @@
 #!/bin/bash
 
-## Script to update all the script folder, 
-## adding new ones if they exist
-## and updating the previous ones if desired
-
 # Constants
 REPO_URL="https://github.com/Deivitto/auditor-docker.git"
 TEMP_DIR=$(mktemp -d)
-LOCAL_SCRIPTS_DIR="$(dirname "$0")"  # We are assuming this script is in the scripts directory
+LOCAL_DIR="$(dirname "$0")/.."  # One level up to cover both scripts and templates
 
 # Clone the repo to the temporary directory
 git clone "$REPO_URL" "$TEMP_DIR"
 
-# Check for differences
-files_to_update=()
-
-for file in "$TEMP_DIR/scripts/"*; do
-    fname=$(basename "$file")
-    if [[ -f "$LOCAL_SCRIPTS_DIR/$fname" ]]; then
-        diff_output=$(diff "$file" "$LOCAL_SCRIPTS_DIR/$fname")
-        if [[ ! -z "$diff_output" ]]; then
+# Function to check differences between local and remote folders
+check_differences() {
+    local folder_name="$1"
+    local files_to_update=()
+    
+    for file in "$TEMP_DIR/$folder_name/"*; do
+        fname=$(basename "$file")
+        if [[ -f "$LOCAL_DIR/$folder_name/$fname" ]]; then
+            diff_output=$(diff "$file" "$LOCAL_DIR/$folder_name/$fname")
+            if [[ ! -z "$diff_output" ]]; then
+                files_to_update+=("$fname")
+            fi
+        else
+            echo "New file $fname detected in $folder_name, will be copied..."
             files_to_update+=("$fname")
         fi
-    else
-        echo "New file $fname detected, will be copied..."
-        files_to_update+=("$fname")
-    fi
-done
+    done
 
-# If there are differences, display them and ask for confirmation
-if [[ ${#files_to_update[@]} -ne 0 ]]; then
-    echo "Differences detected in the following files:"
-    for fname in "${files_to_update[@]}"; do
-        echo "- $fname"
-    done
-    
-    # Get user input
-    while true; do
-        read -p "Do you want to update the files? (y/n) " choice
-        case $choice in
-            [Yy]* ) 
-                for fname in "${files_to_update[@]}"; do
-                    echo "Updating $fname..."
-                    cp "$TEMP_DIR/scripts/$fname" "$LOCAL_SCRIPTS_DIR/$fname"
-                done
-                break
-                ;;
-            [Nn]* )
-                echo "Update cancelled."
-                exit 1
-                ;;
-            * )
-                echo "Please answer with y or n."
-                ;;
-        esac
-    done
-else
-    echo "No differences detected. Your scripts are up to date!"
-fi
+    if [[ ${#files_to_update[@]} -ne 0 ]]; then
+        echo "Differences detected in $folder_name in the following files:"
+        for fname in "${files_to_update[@]}"; do
+            echo "- $fname"
+        done
+
+        # Get user input
+        while true; do
+            read -p "Do you want to update the files in $folder_name? (y/n) " choice
+            case $choice in
+                [Yy]* ) 
+                    for fname in "${files_to_update[@]}"; do
+                        echo "Updating $fname in $folder_name..."
+                        cp "$TEMP_DIR/$folder_name/$fname" "$LOCAL_DIR/$folder_name/$fname"
+                    done
+                    break
+                    ;;
+                [Nn]* )
+                    echo "Update for $folder_name cancelled."
+                    return 1
+                    ;;
+                * )
+                    echo "Please answer with y or n."
+                    ;;
+            esac
+        done
+    else
+        echo "No differences detected in $folder_name. It's up to date!"
+    fi
+}
+
+# Check differences for scripts and templates
+check_differences "scripts"
+check_differences "templates"
 
 # Clean up
 rm -rf "$TEMP_DIR"
