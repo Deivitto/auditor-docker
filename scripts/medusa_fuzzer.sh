cb47167b3575f05544486b499a71d01e34c959ca
Ðavid <info.dcm62@gmail.com>
2023-08-14 14:11:27 +0200

    Create medusa_fuzzer.sh

---
+++ scripts/medusa_fuzzer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Download the Source Code
+echo "Downloading Medusa..."
+wget https://github.com/crytic/medusa/releases/download/v0.1.1/medusa-linux-x64.zip -O medusa.zip
+
+# Extract the Downloaded Archive
+echo "Extracting Medusa..."
+unzip medusa.zip
+
+# Move the Binary to /usr/local/bin/
+echo "Moving Medusa binary to /usr/local/bin/"
+sudo mv medusa /usr/local/bin/
+
+# Make the Binary Executable
+echo "Making Medusa executable..."
+sudo chmod +x /usr/local/bin/medusa
+
+# Cleaning up the downloaded and extracted files
+echo "Cleaning up..."
+rm medusa.zip
+
+echo "Medusa installation complete! You can now use the 'medusa' command from the terminal."
+
