5b400dcd84adcbee3d9e66230b7d99b754e5cacf
Ðavid <info.dcm62@gmail.com>
2023-08-28 19:45:21 +0200

    Update advanced_tob_tools_setup.sh
    Added permalinks and removed wrapping functions

--- scripts/advanced_tob_tools_setup.sh
+++ scripts/advanced_tob_tools_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+
 # Go home
 cd /home/whitehat
 
-mkdir .manticore_env
 # Create a virtual environment for Manticore
 python3.9 -m venv .manticore_env
 
@@ -15,25 +15,16 @@ pip3 install manticore[native] etheno eth-brownie
 # Deactivate the virtual environment after installation
 deactivate
 
-# Append a function to .bashrc for easy usage of Manticore within its virtual environment
-echo -e 'function manticore() {\n
-    source $(pwd)/.manticore_env/bin/activate\n
-    command manticore "$@"\n
-    deactivate\n
-}' >> ~/.bashrc
-
-echo -e 'function etheno() {\n
-    source $(pwd)/.manticore_env/bin/activate\n
-    command etheno "$@"\n
-    deactivate\n
-}' >> ~/.bashrc
-
-
-# Source .bashrc to reflect the changes
-source ~/.bashrc
+# Create symbolic links in ~/.local/bin for manticore and etheno
+ln -sf /home/whitehat/.manticore_env/bin/manticore ~/.local/bin/manticore
+ln -sf /home/whitehat/.manticore_env/bin/etheno ~/.local/bin/etheno
+ln -sf /home/whitehat/.manticore_env/bin/brownie ~/.local/bin/brownie
 
 echo "Manticore installation complete. Use the 'manticore' command to run Manticore within the virtual environment."
-echo "Etheno installed too. Globally installing embark"
+echo "Etheno installed too. Use the 'etheno' command to run Etheno."
+echo "Brownie installed. Use the 'brownie' command to run Eth-Brownie."
+echo "Globally installing embark..."
+
 # Install global npm packages
 npm install --omit=dev --global --force \
     embark \
